cb14e2f0cc0cd2ec321018dcdbb7f2a1fc3a8f1c
Márk <donkomark@gmail.com>
2020-01-17 20:28:02 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Verem_Algo2/Verem/Elem.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verem
+{
+    class Elem<T>
+    {
+        private T[] adat;
+        public T[] Adat
+        {
+            get { return adat; }
+            set { adat = value; }
+        }
+
+        private int elemszam;
+        public int Elemszam
+        {
+            get { return elemszam; }
+            set { elemszam = value; }
+        }
+
+        int maxMeret;
+        public int Maxmeret
+        {
+            get { return maxMeret; }
+            set { maxMeret = value; }
+        }
+
+        public Elem(int Maxdb)
+        {
+            Elemszam = 0;
+            Maxmeret = Maxdb;
+            Adat = new T[Maxdb];
+        }
+        public void Push (T ertek)
+        {
+            if (Elemszam == Maxmeret)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            Adat[elemszam] = ertek;
+            elemszam++;
+        }
+        public T Peek()
+        {
+            if (Elemszam == 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            Elemszam--;
+            return Adat[Elemszam];
+        }
+    }
+}
---
+++ Verem_Algo2/Verem/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elem<int> szamok = new Elem<int>(3);
+            Elem<string> szavak = new Elem<string>(3);
+            szamok.Push(1);
+            szamok.Push(2);
+            szamok.Push(3);
+            Console.WriteLine(szamok.Elemszam);
+            Console.ReadLine();
+            szamok.Peek();
+            Console.WriteLine(szamok.Elemszam);
+            Console.ReadLine();
+            szavak.Push("Bela");
+            szavak.Push("Bela");
+            szavak.Push("Bela");
+            Console.WriteLine(szavak.Elemszam);
+            Console.ReadLine();
+            szavak.Peek();
+            Console.WriteLine(szavak.Elemszam);
+            Console.ReadLine();
+        }
+    }
+}
